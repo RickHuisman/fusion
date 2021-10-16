376af2a536216f16ecc7e86b1892dd6a29fd15a4
rickhuis <rick.huisman@live.nl>
2021-10-16 22:02:25 +0200

    Add repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    repl();
+}
+
+fn repl() {
+    loop {
+        println!("{}", &read_line());
+    }
+}
+
+fn read_line() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            trim_newline(&mut input);
+            input
+        }
+        Err(error) => {
+            panic!("error: {}", error);
+        }
+    }
+}
+
+fn trim_newline(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
+    }
 }
